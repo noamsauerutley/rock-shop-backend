da50162b62392fc5534dc3851fd582238c9c8acb
github username <github email address>
2019-12-06 10:53:46 -0500

    bugs

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ class LoginController < ApplicationController
 
     def create
       user = User.find_by("lower(username) = ?", params[:username].downcase)
-      byebug
+      # byebug
       if user && user.authenticate(params[:password])
         render json: { token: token(user.id), total: user.orders.last.showTotal, purchases: user.orders.last.showPurchaseRocks, user_id: user.id, order_id: user.orders.last.id }
       else
